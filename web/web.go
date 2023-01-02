572b9ab5f2d95216445e7090fd8aad2771b159ee
Gabriel P. Bezerra <gabriel.bezerra@ufv.br>
2023-01-02 16:57:21 -0300

    Adiciona OpenAPI docs

--- web/web.go
+++ web/web.go
@@ -12,12 +12,19 @@ import (
 func SetWebHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/", generateOtpHandler)
 	mux.HandleFunc("/encode", encodeSecretHandler)
+	mux.HandleFunc("/openapi", showOpenAPIDocs)
 }
 
 func generateOtpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "POST is the only method allowed for this endpoint")
+		return
+	}
 	err := r.ParseForm()
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error parsing the request: %s", err.Error())
 		log.Println("ERROR: Error parsing the request " + err.Error())
 		return
 	}
@@ -25,6 +32,7 @@ func generateOtpHandler(w http.ResponseWriter, r *http.Request) {
 	secret := r.PostForm.Get("secret")
 	if secret == "" {
 		log.Println("Request doesn't sent any secret. Abort OTP generation")
+		fmt.Fprintln(w, "Parameter \"secret\" was not provided. Secret is required")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -41,7 +49,23 @@ func generateOtpHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func encodeSecretHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "GET is the only method allowed for this endpoint")
+		return
+	}
+
 	secret := r.URL.Query().Get("secret")
 	log.Printf("Secret %s", secret)
+	if secret == "" {
+		log.Println("Request doesn't sent any secret. Abort OTP generation")
+		fmt.Fprintln(w, "Parameter \"secret\" was not provided. Secret is required")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	fmt.Fprintln(w, encoding.EncodeSecret(secret))
+}
+
+func showOpenAPIDocs(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./web/openapi.json")
 }
